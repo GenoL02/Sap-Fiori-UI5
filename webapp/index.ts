d812bdae8bb73b333b4275381db6ba2fb62c7dbd
GenoL02 <duongthanhlong02@gmail.com>
2025-04-17 14:28:14 +0700

    first commit for fiori ui5

---
+++ webapp/index.ts
@@ -0,0 +1,9 @@
+import ComponentContainer from "sap/ui/core/ComponentContainer";
+
+new ComponentContainer({
+    name: "ui5.walkthrough",
+    settings : {
+        id : "walkthrough"
+    },
+    async: true
+}).placeAt("content");
